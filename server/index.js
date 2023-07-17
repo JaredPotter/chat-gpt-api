9243388a064ebdb579176b9400920f0733f5d32e
Jared Potter <jaredpotter1@gmail.com>
2023-07-16 18:11:13 -0600

    Add Readme, add server pm2 forever node, and server updates.

--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
-const puppeteer = require("puppeteer");
-const express = require("express");
-const fs = require("fs-extra");
-const axios = require("axios");
-const { spawn, spawnSync } = require("child_process");
-const asyncLock = require("async-lock");
+const puppeteer = require('puppeteer');
+const express = require('express');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { spawn, spawnSync } = require('child_process');
+const asyncLock = require('async-lock');
 const lock = new asyncLock();
 
-fs.ensureDirSync("../cookies");
+fs.ensureDirSync('../cookies');
 
 const app = express();
 app.use(express.json());
 const puppeteerTimeout = 90000;
-const COOKIES_PATH = "../cookies/latest_cookies.json";
+const COOKIES_PATH = '../cookies/latest_cookies.json';
 const WINDOW_HEIGHT = 1000;
 const WINDOW_WIDTH = 1000;
 let page;
 
-app.post("/api/chat", async (request, response) => {
+app.post('/api/chat', async (request, response) => {
   const query = request.body.query;
   console.log(`/api/chat CALLED - query: ${query}`);
-  let responseMessage = "";
+  let responseMessage = '';
 
   try {
-    await lock.acquire("resourceLock", async (done) => {
+    await lock.acquire('resourceLock', async (done) => {
       responseMessage = await queryChatGpt(page, query);
       done();
     });
   } catch (err) {
     console.error(err);
-    response.status(500).send("Failed to acquire lock");
+    response.status(500).send('Failed to acquire lock');
     return;
   }
 
@@ -37,7 +37,7 @@ app.post("/api/chat", async (request, response) => {
   });
 });
 
-app.get("/api/set-cookies", async (request, response) => {
+app.get('/api/set-cookies', async (request, response) => {
   console.log(`/api/set-cookies CALLED`);
 
   await openChrome();
@@ -46,38 +46,38 @@ app.get("/api/set-cookies", async (request, response) => {
     await openChatGpt();
   } catch (error) {
     response.send(
-      "Failed to set cookies. Please login and recall GET /api/set-cookies endpoint."
+      'Failed to set cookies. Please login and recall GET /api/set-cookies endpoint.'
     );
     return;
   }
 
-  console.log("Successfully set cookies");
-  response.send("Successfully set cookies");
+  console.log('Successfully set cookies');
+  response.send('Successfully set cookies');
 });
 
 async function openChatGpt() {
-  console.log("Opening chat.openai.com...");
+  console.log('Opening chat.openai.com...');
 
-  await page.goto("https://chat.openai.com/");
+  await page.goto('https://chat.openai.com/');
 
-  let cookies = "";
+  let cookies = '';
 
   try {
     cookies = JSON.parse(
       fs.readFileSync(COOKIES_PATH, {
-        encoding: "utf-8",
+        encoding: 'utf-8',
       })
     );
   } catch (error) {
     console.error(error);
-    console.error("Failed to find latest_cookies.json.");
-    console.error("Attempting to get cookies.");
+    console.error('Failed to find latest_cookies.json.');
+    console.error('Attempting to get cookies.');
 
     try {
-      await page.waitForSelector("#prompt-textarea");
+      await page.waitForSelector('#prompt-textarea');
     } catch (error) {
       console.error(
-        "Please login into chatGPT and re-call /api/set-cookies endpoint."
+        'Please login into chatGPT and re-call /api/set-cookies endpoint.'
       );
       throw error;
     }
@@ -89,12 +89,12 @@ async function openChatGpt() {
     }
 
     await page.setUserAgent(
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
     );
-    await page.goto("https://chat.openai.com/");
+    await page.goto('https://chat.openai.com/');
     const client = await page.target().createCDPSession();
-    await client.send("Network.clearBrowserCache");
-    await page.reload({ waitUntil: "networkidle0" });
+    await client.send('Network.clearBrowserCache');
+    await page.reload({ waitUntil: 'networkidle0' });
     const refreshedCookies = await page.cookies();
     fs.writeFile(
       COOKIES_PATH,
@@ -104,7 +104,7 @@ async function openChatGpt() {
 
     // Delete welcome modal
     await page.evaluate(() => {
-      const element = document.querySelector(".absolute.inset-0");
+      const element = document.querySelector('.absolute.inset-0');
       if (element) {
         element.remove();
       }
@@ -122,12 +122,12 @@ async function queryChatGpt(
 ) {
   let isJsonResponse = false;
 
-  if (message.includes("code block")) {
+  if (message.includes('code block')) {
     isJsonResponse = true;
   }
 
   const messageTextboxSelector = await page.waitForSelector(
-    "#prompt-textarea",
+    '#prompt-textarea',
     {
       timeout: puppeteerTimeout,
     }
@@ -137,12 +137,12 @@ async function queryChatGpt(
     (selector, value) => {
       document.querySelector(selector).value = value;
     },
-    "#prompt-textarea",
+    '#prompt-textarea',
     message
   );
 
-  await messageTextboxSelector.type(" ");
-  await page.keyboard.press("Enter");
+  await messageTextboxSelector.type(' ');
+  await page.keyboard.press('Enter');
 
   const regenerateButtonXPath =
     '//button[@as="button"]//div[contains(text(), "Regenerate response")]';
@@ -150,31 +150,30 @@ async function queryChatGpt(
 
   const responseMessage = await page.evaluate(async (isJsonResponse) => {
     const chatMessages = document.querySelectorAll(
-      ".group.w-full.text-gray-800"
+      '.group.w-full.text-gray-800'
     );
     let mostRecentMessage = chatMessages[chatMessages.length - 1];
-    let resultMessage = "";
-
+    let resultMessage = '';
     // Attempt to get a <code> block.
     if (isJsonResponse) {
-      const responseMessageCodeSelector = "#__next code";
+      const responseMessageCodeSelector = '#__next code';
       const codeElement = mostRecentMessage.querySelector(
         responseMessageCodeSelector
       );
 
       try {
         resultMessage = JSON.parse(codeElement.textContent);
+        // throw 'blah';
       } catch (error) {
         // Attempt to get JSON via manual parsing.
-        const messageElements = document.querySelectorAll(".markdown.prose");
         const mostRecentMessageElement =
-          messageElements[messageElements.length - 1];
+          mostRecentMessage.querySelector('.markdown.prose');
         mostRecentMessage = mostRecentMessageElement.textContent;
 
         try {
           const startIndices = [
-            mostRecentMessage.indexOf("{"),
-            mostRecentMessage.indexOf("["),
+            mostRecentMessage.indexOf('{'),
+            mostRecentMessage.indexOf('['),
           ];
           const validStartIndex = Math.min(...startIndices.filter(Boolean));
 
@@ -182,13 +181,13 @@ async function queryChatGpt(
             let depth = 0;
             for (let i = validStartIndex; i < mostRecentMessage.length; i++) {
               if (
-                mostRecentMessage[i] === "{" ||
-                mostRecentMessage[i] === "["
+                mostRecentMessage[i] === '{' ||
+                mostRecentMessage[i] === '['
               ) {
                 depth++;
               } else if (
-                mostRecentMessage[i] === "}" ||
-                mostRecentMessage[i] === "]"
+                mostRecentMessage[i] === '}' ||
+                mostRecentMessage[i] === ']'
               ) {
                 depth--;
                 if (depth === 0) {
@@ -198,8 +197,7 @@ async function queryChatGpt(
                     );
                     break;
                   } catch (e) {
-                    console.log("Invalid JSON detected");
-                    debugger;
+                    console.log('Invalid JSON detected');
                   }
                 }
               }
@@ -212,12 +210,12 @@ async function queryChatGpt(
       }
     } else {
       const responseMessageSelector =
-        "#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div > div > main > div.flex-1.overflow-hidden > div > div > div > div > div > div.gap-1";
+        '#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div > div > main > div.flex-1.overflow-hidden > div > div > div > div > div > div.gap-1';
       const messages = document.querySelectorAll(responseMessageSelector);
       const mostRecentMessageNode = messages[messages.length - 1];
       resultMessage = mostRecentMessageNode.textContent;
 
-      resultMessage = resultMessage.replace("1 / 1", "");
+      resultMessage = resultMessage.replace('1 / 1', '');
     }
 
     return resultMessage;
@@ -232,24 +230,24 @@ async function extractJSON(str) {
   if (match) {
     try {
       var obj = JSON.parse(match[0]); // Try to parse the matched string
-      if (obj && typeof obj === "object") {
+      if (obj && typeof obj === 'object') {
         return obj; // If parse is successful, return the object
       }
     } catch (e) {
-      console.log("Invalid JSON detected");
+      console.log('Invalid JSON detected');
     }
   }
   return null;
 }
 
 async function openChrome() {
-  console.log("Running on Mac");
-  spawnSync("killall", [`Google Chrome`]);
+  console.log('Running on Mac');
+  spawnSync('killall', [`Google Chrome`]);
   chromeLauncher = `/Applications/Google Chrome.app/Contents/MacOS/Google Chrome`;
   chromeLauncherFlags = [
-    "--remote-debugging-port=9222",
-    "--no-first-run",
-    "--no-default-browser-check",
+    '--remote-debugging-port=9222',
+    '--no-first-run',
+    '--no-default-browser-check',
     `--window-size=${WINDOW_WIDTH},${WINDOW_HEIGHT}`,
     // `--user-data-dir=$(mktemp -d -t "chrome-remote_data_dir)"`,
   ];
@@ -259,7 +257,7 @@ async function openChrome() {
   );
 
   if (!wsChromeEndpointUrl) {
-    console.log("Failed to load websocket URL. Exiting now!");
+    console.log('Failed to load websocket URL. Exiting now!');
     return;
   }
 
@@ -269,13 +267,13 @@ async function openChrome() {
     ignoreHTTPSErrors: true,
     slowMo: 0,
     args: [
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--no-first-run",
-      "--no-sandbox",
-      "--no-zygote",
-      "--window-size=1280,720",
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--window-size=1280,720',
     ],
   });
 
@@ -283,10 +281,10 @@ async function openChrome() {
 }
 
 async function closeChrome() {
-  if (process.platform === "win32") {
-    crossSpawnSync("powershell", ["kill", "-n", "chrome"]);
-  } else if (process.platform === "darwin" || process.platform === "linux") {
-    crossSpawnSync("killall", [`Google Chrome`]);
+  if (process.platform === 'win32') {
+    crossSpawnSync('powershell', ['kill', '-n', 'chrome']);
+  } else if (process.platform === 'darwin' || process.platform === 'linux') {
+    crossSpawnSync('killall', [`Google Chrome`]);
   }
 }
 
@@ -294,11 +292,11 @@ async function startChromeProcess(chromeLauncher, chromeLauncherFlags) {
   // Starts Chrome
   try {
     const chromeStartCommand = `${chromeLauncher} ${chromeLauncherFlags.join(
-      " "
+      ' '
     )}`;
     console.log(`Running \n${chromeStartCommand}`);
 
-    spawn(chromeLauncher, chromeLauncherFlags, { stdio: "inherit" });
+    spawn(chromeLauncher, chromeLauncherFlags, { stdio: 'inherit' });
   } catch (error) {
     // do nothing.
   }
@@ -306,15 +304,15 @@ async function startChromeProcess(chromeLauncher, chromeLauncherFlags) {
   await sleep(2500);
 
   try {
-    const url = "http://127.0.0.1:9222/json/version";
-    console.log("Fetching webSocket URL... " + url);
+    const url = 'http://127.0.0.1:9222/json/version';
+    console.log('Fetching webSocket URL... ' + url);
     const response = await axios.get(url);
     const data = response.data;
     return data.webSocketDebuggerUrl;
   } catch (error) {
     console.error(error);
-    console.error("error - " + JSON.stringify(error, null, 4));
-    console.log("Request failed. Exiting now.");
+    console.error('error - ' + JSON.stringify(error, null, 4));
+    console.log('Request failed. Exiting now.');
     return;
   }
 }
@@ -326,7 +324,7 @@ async function sleep(ms) {
 }
 
 (async () => {
-  if (process.argv[2] === "isDev") {
+  if (process.argv[2] === 'isDev') {
     // const message = await openChatGpt();
     // console.log(message);
   }
@@ -335,6 +333,6 @@ async function sleep(ms) {
   await openChatGpt();
 
   app.listen(3000, () => {
-    console.log("Server Started");
+    console.log('Server Started');
   });
 })();
